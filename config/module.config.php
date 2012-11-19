952824172e6fdf2e6d59faedda29cd863c0e7fd7
kuzmar <info@balavec.net>
2012-11-19 18:45:08 +0100

    adding the rest of the functions

---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Facebook\Controller\Facebook' => 'Facebook\Controller\FacebookController',
+        	'Facebook\Controller\BaseFacebook' => 'Facebook\Controller\BaseFacebookController',
+        ),
+    ),
+);
